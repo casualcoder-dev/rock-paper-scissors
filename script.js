583d2351b195e5ac4f363d0151659a70440a26c5
Junaid Raza <just.junaid.rana@gmail.com>
2023-06-21 22:11:39 +0500

    version two

--- script.js
+++ script.js
@@ -8,49 +8,44 @@ function getComputerChoice() {
   } else return "Scissors"
 };
 
-let computerSelection = getComputerChoice();
-let playerSelection = prompt("Choose your weapon!")
+
 let playerScore = 1;
 let computerScore = 1;
 
 function playRound(playerSelection, computerSelection) {
        if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "Paper") {
         computerScore++
-        console.log("You Lose! Paper beats Rock")
-  return "You Lose! Paper beats Rock"
+        alert("You Lose! Paper beats Rock")
 } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "Scissors") {
   computerScore++
-  console.log("You Lose! Paper beats Rock")
-  return "You Lose! Scissors beats Paper"
+  alert("You Lose! Scissors beats Paper")
 } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "Rock") {
   computerScore++
-  console.log("You Lose! Paper beats Rock")
-  return "You Lose! Rock beats Scissors"
+  alert("You Lose! Rock beats Scissors")
 } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "Scissors") {
   playerScore++
-  console.log("You Win! Rock beats Scissors")
-  return "You Win! Rock beats Scissors"
+  alert("You Win! Rock beats Scissors")
 } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "Rock") {
   playerScore++
-  console.log("You Win! Rock beats Scissors")
-  return "You Win! Paper beats Rock"
+  alert("You Win! Paper Beats Rock")
 } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "Paper") {
   playerScore++
-  console.log("You Win! Rock beats Scissors")
-  return "You Win! Scissors beats Paper"
-} else return "Its a tie!"
+  alert("You Win! Scissors beats Paper")
+} else alert("Its a tie!")
 };
 // console.log(playRound(playerSelection,computerSelection)) 
 
 function game() {
-  playRound(playerSelection,getComputerChoice())
-  playRound(playerSelection,getComputerChoice())
-  playRound(playerSelection,getComputerChoice())
-  playRound(playerSelection,getComputerChoice())
-  playRound(playerSelection,getComputerChoice())
+  playRound(prompt("Choose your weapon!"),getComputerChoice())
+  playRound(prompt("Choose your weapon!"),getComputerChoice())
+  playRound(prompt("Choose your weapon!"),getComputerChoice())
+  playRound(prompt("Choose your weapon!"),getComputerChoice())
+  playRound(prompt("Choose your weapon!"),getComputerChoice())
   if (playerScore > computerScore) {
     console.log("You Win!!!")
-  } else console.log("You Lose!!!")
+  } else if (playerScore === computerScore) {
+    console.log("Its a tie!!!")
+  } else console.log('You Lose!!!')
 };
 
 game();
